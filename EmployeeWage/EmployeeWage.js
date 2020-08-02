a6efed3f1dec1cfe73dd8389ea04fdeaf78bce8a
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-02 21:17:54 +0530

    REFACTORED: Use of switch case instead of if else if statements.

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -4,17 +4,18 @@ let empCheck = Math.floor(Math.random() * 10) % 3;
 let WAGE_PER_HOUR = 20;
 let hoursPerDay = 0;
 let wageForOneDay = 0;
-if (empCheck == IS_FULL_TIME)
+switch (empCheck)
 {
-	console.log("Employee is Present and is Full Time");
-	hoursPerDay = 8;
+	case IS_FULL_TIME:
+		console.log("Employee is Present and is Full Time");
+		hoursPerDay = 8;
+	break;
+	case IS_PART_TIME:
+		console.log("Employee is Present and is Part Time");
+		hoursPerDay = 4;
+	break;
+	default:
+		console.log("Employee is Absent");
 }
-else if (empCheck == IS_PART_TIME)
-{
-	console.log("Employee is Present and is Part Time");
-	hoursPerDay = 4
-}
-else
-	console.log("Employee is Absent");
 wageForOneDay = hoursPerDay * WAGE_PER_HOUR;
 console.log("Today's Wage :" + wageForOneDay);
