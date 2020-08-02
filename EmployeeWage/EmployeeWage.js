87ec0606b9279f8c52666edad80d38d649b28b6a
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-02 21:45:39 +0530

    ADDED: Calculation of wage for a month of twenty working days.

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -1,21 +1,24 @@
 let IS_FULL_TIME = 1;
+let FULL_TIME_DAY_HOURS = 8;
 let IS_PART_TIME = 2;
-let empCheck = Math.floor(Math.random() * 10) % 3;
+let PART_TIME_DAY_HOURS = 4;
 let WAGE_PER_HOUR = 20;
-let hoursPerDay = 0;
-let wageForOneDay = 0;
-switch (empCheck)
+let NUM_OF_WORKING_DAYS = 20;
+let hoursInMonth = 0;
+let wageForMonth = 0;
+for (let dayCount = 1; dayCount <= NUM_OF_WORKING_DAYS; dayCount++)
 {
-	case IS_FULL_TIME:
-		console.log("Employee is Present and is Full Time");
-		hoursPerDay = 8;
-	break;
-	case IS_PART_TIME:
-		console.log("Employee is Present and is Part Time");
-		hoursPerDay = 4;
-	break;
-	default:
-		console.log("Employee is Absent");
+	let empCheck = Math.floor(Math.random() * 10) % 3;
+	switch (empCheck)
+	{
+		case IS_FULL_TIME:
+			hoursInMonth += FULL_TIME_DAY_HOURS;
+		break;
+		case IS_PART_TIME:
+			hoursInMonth += PART_TIME_DAY_HOURS;
+		break;
+	}
 }
-wageForOneDay = hoursPerDay * WAGE_PER_HOUR;
-console.log("Today's Wage :" + wageForOneDay);
+wageForMonth = hoursInMonth * WAGE_PER_HOUR;
+console.log("Total Working Hours :" + hoursInMonth);
+console.log("Total Wage :" + wageForMonth);
