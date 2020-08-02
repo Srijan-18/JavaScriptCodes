bf4de0a83c848d45b20dae312e7f73baf4b1294f
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-02 21:12:21 +0530

    ADDED: Part time daily wage computation for employee.

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -1,13 +1,20 @@
-let isPresent = 1;
-let empCheck = Math.floor(Math.random() * 10) % 2;
+let IS_FULL_TIME = 1;
+let IS_PART_TIME = 2;
+let empCheck = Math.floor(Math.random() * 10) % 3;
 let WAGE_PER_HOUR = 20;
-let HOURS_PER_DAY = 8;
+let hoursPerDay = 0;
 let wageForOneDay = 0;
-if (empCheck == isPresent)
+if (empCheck == IS_FULL_TIME)
 {
-	console.log("Employee is Present");
-	wageForOneDay = WAGE_PER_HOUR * HOURS_PER_DAY;
+	console.log("Employee is Present and is Full Time");
+	hoursPerDay = 8;
+}
+else if (empCheck == IS_PART_TIME)
+{
+	console.log("Employee is Present and is Part Time");
+	hoursPerDay = 4
 }
 else
 	console.log("Employee is Absent");
+wageForOneDay = hoursPerDay * WAGE_PER_HOUR;
 console.log("Today's Wage :" + wageForOneDay);
