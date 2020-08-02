0c68acbd5ca4ec6f1860af09042f4064a2afff2a
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-02 23:52:22 +0530

    ADDED: Code to store daily wages along with total monthly wage.

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -5,30 +5,36 @@ let PART_TIME_DAY_HOURS = 4;
 let WAGE_PER_HOUR = 20;
 let MAX_WORKING_DAYS = 20;
 let MAX_WORKING_HOURS = 100;
-let hoursInMonth = 0;
+let hoursWorkedInMonth = 0;
 let numOfDays = 0;
+let dailyWagesOfEmployee = new Array();
 
 function getDailyWorkingHours() {
-let dailyHours = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-	switch (empCheck)
-	{
-		case IS_FULL_TIME:
-			dailyHours = FULL_TIME_DAY_HOURS;
-		break;
-		case IS_PART_TIME:
-			dailyHours = PART_TIME_DAY_HOURS;
-		break;
-	}
-	return dailyHours;
+	let dailyHours = 0;
+	let empCheck = Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+			case IS_FULL_TIME:
+				dailyHours = FULL_TIME_DAY_HOURS;
+			break;
+			case IS_PART_TIME:
+				dailyHours = PART_TIME_DAY_HOURS;
+			break;
+		}
+		return dailyHours;
 }
 
-while( numOfDays < MAX_WORKING_DAYS && hoursInMonth < MAX_WORKING_HOURS)
-{
-	hoursInMonth += getDailyWorkingHours();
-	numOfDays++;
+function calculateWage(workingHours) {
+	return WAGE_PER_HOUR * workingHours;
 }
-let wageForMonth = hoursInMonth * WAGE_PER_HOUR;
+
+while( numOfDays < MAX_WORKING_DAYS && hoursWorkedInMonth < MAX_WORKING_HOURS) {
+	let dailyHours = getDailyWorkingHours();
+	hoursWorkedInMonth += dailyHours;
+	numOfDays++;
+	dailyWagesOfEmployee.push(calculateWage(dailyHours));
+}
+
+let wageForMonth = calculateWage(hoursWorkedInMonth);
 console.log("Num of Days :" + numOfDays);
-console.log("Total Working Hours :" + hoursInMonth);
+console.log("Total Working Hours :" + hoursWorkedInMonth);
 console.log("Total Wage :" + wageForMonth);
