c77b7aa68e7d9a4959ab143355c8101f4cd69fdd
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-02 21:00:11 +0530

    ADDED: Calculation of daily wage of employee.

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -1,6 +1,13 @@
 let isPresent = 1;
 let empCheck = Math.floor(Math.random() * 10) % 2;
+let WAGE_PER_HOUR = 20;
+let HOURS_PER_DAY = 8;
+let wageForOneDay = 0;
 if (empCheck == isPresent)
+{
 	console.log("Employee is Present");
+	wageForOneDay = WAGE_PER_HOUR * HOURS_PER_DAY;
+}
 else
 	console.log("Employee is Absent");
+console.log("Today's Wage :" + wageForOneDay);
