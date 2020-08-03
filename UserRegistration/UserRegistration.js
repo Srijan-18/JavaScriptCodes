46a5be6d3383529d19ac1f9e1ac5059d0326da33
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-03 07:38:11 +0530

    ADDED: User input and its validation for last name.

--- UserRegistration/UserRegistration.js
+++ UserRegistration/UserRegistration.js
@@ -1,6 +1,20 @@
 let prompt = require('prompt-sync')();
 
-let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-let name = prompt("Enter your name :");
-let checkName = nameRegex.test(name);
-console.log(checkName);
+let NAME_REGEX = RegExp('^[A-Z]{1}[a-z]{2,}$');
+
+function resultDisplay(result) {
+if(result)
+	console.log("Valid First Name.\n");
+else
+	console.log("Invalid First Name.\n");
+}
+
+//========== FIRST NAME CHECK ==========
+let input = prompt("Enter First Name :");
+let result = NAME_REGEX.test(input);
+resultDisplay(result);
+
+//========== LAST NAME CHECK ==========
+input = prompt("Enter Last Name :");
+result = NAME_REGEX.test(input);
+resultDisplay(result);
