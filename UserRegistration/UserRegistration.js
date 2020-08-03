4c7940f41881b40a920c94668e841243894fd87f
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-03 07:49:50 +0530

    ADDED: User input and its validation for Email-Id.

--- UserRegistration/UserRegistration.js
+++ UserRegistration/UserRegistration.js
@@ -1,20 +1,28 @@
 let prompt = require('prompt-sync')();
 
 let NAME_REGEX = RegExp('^[A-Z]{1}[a-z]{2,}$');
+let EMAIL_REGEX = RegExp('^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,3})?$');
 
-function resultDisplay(result) {
+function resultDisplay(result, field) {
 if(result)
-	console.log("Valid First Name.\n");
+	console.log("Valid "+ field + ".\n");
 else
-	console.log("Invalid First Name.\n");
+	console.log("Invalid "+ field + ".\n");
 }
 
 //========== FIRST NAME CHECK ==========
 let input = prompt("Enter First Name :");
 let result = NAME_REGEX.test(input);
-resultDisplay(result);
+resultDisplay(result, "First Name");
 
 //========== LAST NAME CHECK ==========
 input = prompt("Enter Last Name :");
 result = NAME_REGEX.test(input);
-resultDisplay(result);
+resultDisplay(result, "Last Name");
+
+//========== E-MAIL CHECK ==========
+input = prompt("Enter Email-Id :");
+result = EMAIL_REGEX.test(input);
+resultDisplay(result, "Email-Id");
+
+
