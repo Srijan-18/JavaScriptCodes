a82d514fb97dab82c62199848b4cb5d11488be24
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-03 08:07:47 +0530

    ADDED: User input and its validation for Password for condition of min 8 chacraters.

--- UserRegistration/UserRegistration.js
+++ UserRegistration/UserRegistration.js
@@ -3,6 +3,8 @@ let prompt = require('prompt-sync')();
 let NAME_REGEX = RegExp('^[A-Z]{1}[a-z]{2,}$');
 let EMAIL_REGEX = RegExp('^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,3})?$');
 let MOBILE_NUMBER_REGEX = RegExp('^[1-9][0-9]{0,2}[ ][1-9][0-9]{9}$');
+let PASSWORD_REGEX = RegExp('^([A-z0-9]){8,}$');
+
 function resultDisplay(result, field) {
 if(result)
 	console.log("Valid "+ field + ".\n");
@@ -29,3 +31,8 @@ resultDisplay(result, "Email-Id");
 input = prompt("Enter Mobile Number :");
 result = MOBILE_NUMBER_REGEX.test(input);
 resultDisplay(result, "Mobile Number");
+
+//========== PASSWORD ==========
+input = prompt("Enter Password :");
+result = PASSWORD_REGEX.test(input);
+resultDisplay(result, "Password");
