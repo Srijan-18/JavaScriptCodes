fb28cbff080b63c80737036534416861d8c2b3f2
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-03 08:00:41 +0530

    ADDED: User input and its validation for Mobile Number.

--- UserRegistration/UserRegistration.js
+++ UserRegistration/UserRegistration.js
@@ -2,7 +2,7 @@ let prompt = require('prompt-sync')();
 
 let NAME_REGEX = RegExp('^[A-Z]{1}[a-z]{2,}$');
 let EMAIL_REGEX = RegExp('^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,3})?$');
-
+let MOBILE_NUMBER_REGEX = RegExp('^[1-9][0-9]{0,2}[ ][1-9][0-9]{9}$');
 function resultDisplay(result, field) {
 if(result)
 	console.log("Valid "+ field + ".\n");
@@ -10,19 +10,22 @@ else
 	console.log("Invalid "+ field + ".\n");
 }
 
-//========== FIRST NAME CHECK ==========
+//========== FIRST NAME ==========
 let input = prompt("Enter First Name :");
 let result = NAME_REGEX.test(input);
 resultDisplay(result, "First Name");
 
-//========== LAST NAME CHECK ==========
+//========== LAST NAME ==========
 input = prompt("Enter Last Name :");
 result = NAME_REGEX.test(input);
 resultDisplay(result, "Last Name");
 
-//========== E-MAIL CHECK ==========
+//========== E-MAIL ==========
 input = prompt("Enter Email-Id :");
 result = EMAIL_REGEX.test(input);
 resultDisplay(result, "Email-Id");
 
-
+//========== MOBILE NUMBER ===========
+input = prompt("Enter Mobile Number :");
+result = MOBILE_NUMBER_REGEX.test(input);
+resultDisplay(result, "Mobile Number");
